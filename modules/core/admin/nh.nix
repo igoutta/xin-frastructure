bd9f16b72cf029c5f8ecf29df5af7b4e7dbb3da8
igoutta <igoutta@users.noreply.github.com>
2025-10-13 15:02:33 -0500

    fix: wine

--- modules/core/admin/nh.nix
+++ modules/core/admin/nh.nix
@@ -1,7 +1,4 @@
-{
-  inputs,
-  ...
-}:
+{ inputs, ... }:
 {
   imports = [
     inputs.make-shell.flakeModules.default
--- modules/services/software/virtualisation/wine.nix
+++ modules/services/software/virtualisation/wine.nix
@@ -1,10 +1,31 @@
 {
-  flake.modules.nixos.virtualisation = {
-    environment.systemPackages = with pkgs; [
+  flake.modules.homeManager.virtualisation = {
+    home.packages = with pkgs; [
       wineWowPackages.yabridge
       wineWowPackages.fonts
       winetricks
       q4wine
     ]; #TODO: make shells and settings
+
+    programs.lutris.enable = true;
+    #services.wine = {
+    #  enable = true;
+    #  settings = {
+    #    yabridge = {
+    #      enable = true;
+    #      autoImport = true;
+    #      autoUpdate = true;
+    #      autoExport = true;
+    #      use32Bit = true;
+    #      use64Bit = true;
+    #      useStaging = false;
+    #      useEsync = true;
+    #      useFsync = false;
+    #      debug = false;
+    #      verbose = false; # very verbose logging
+    #      dllOverrides = [ "xaudio2_7=n,b" "d3dcompiler_43=n,b" "d3d11=n,b" "dxgi=n,b  " ];
+    #    };
+    #  };
+    #
   };
 }
